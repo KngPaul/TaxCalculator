67e13da8674e1eb598e8fa9cfc5f3892db1fd000
Paul Onawola <onawola.ire@gmail.com>
2023-01-03 19:43:21 +0100

    Initial Commit

---
+++ TaxCalculator/TaxCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxCalculatorApp.swift
+//  TaxCalculator
+//
+//  Created by Paul Onawola on 03/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TaxCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
