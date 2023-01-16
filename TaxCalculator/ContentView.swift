5cd7f7cb33583721786dd6aab3a7e7807408cbbb
Paul Onawola <onawola.ire@gmail.com>
2023-01-16 19:53:08 +0100

    Project #1/52. Tax Calculator is an iOS app for deducing the tax to pay based on income using the Nigerian tax scheme

--- TaxCalculator/ContentView.swift
+++ TaxCalculator/ContentView.swift
@@ -8,15 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    
+    @State private var salary: Double?
+    private var tax: Double {
+        var base = salary ?? 0.00
+        var instance = 0.00
+        
+        if base < 300000 {
+            instance = base * 0.01
+            return instance
         }
-        .padding()
+        
+        if base >= 300000 {
+            base -= 300000
+            instance += 21000
+            if base < 300000 {
+                return instance
+            }
+        }
+        
+        if base >= 300000 {
+            base -= 300000
+            instance += 33000
+            if base < 500000 {
+                return instance
+            }
+        }
+        
+        if base >= 500000 {
+            base -= 500000
+            instance += 75000
+            if base < 500000 {
+                return instance
+            }
+        }
+        
+        if base >= 500000 {
+            base -= 500000
+            instance += 95000
+            if base < 1600000 {
+                return instance
+            }
+        }
+        
+        if base >= 1600000 {
+            base -= 1600000
+            instance += 240240
+            if base > 0 {
+                instance += (base * 0.24)
+                return instance
+            } else {
+                return instance
+            }
+        }
+        
+        return instance
     }
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Section {
+                    Text(tax, format: .currency(code: "NGN"))
+                        .font(.largeTitle)
+                }
+                .padding(.top)
+                Form {
+                    TextField("Salary", value: $salary, format: .currency(code: "NGN"))
+                }
+                
+            }
+            .navigationBarTitle("Tax Calculator")
+        }
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
